d2eaf540e0ce9c70ead38fdbe441047596ddc452
Greg <buthker.2@wright.edu>
2018-07-26 09:27:46 -0400

    Parser fixes

--- pysensor.py
+++ pysensor.py
@@ -4,38 +4,42 @@ import json
 import jsongraph
 from pprint import pprint
 
+# parse func information from JSON file
 def parse_json(json_file):
     # TODO: parse JSON file, load into a data structure for analysis
-    print("Not implemented yet!")
     with open(json_file) as f: #first, pull in json file
        graph = json.load(f)
     print(json.dumps(graph)) # dump graph, TODO: parse graph
     return graph
 
-def parse_rom():
+# parse json information from ROM
+def parse_rom(infile):
     # TODO: include R_PIPE API, call R2 and get the JSON from the ROM
     print("Not implemented yet!")
+    ret = infile
+    return ret
 
 def main ():
     # set up the parser first
     # default parser args - filename, opens file for JSON parsing 
     # can also output JSON file as a .DOT file, or pull in a ROM and call R2 
     parser = argparse.ArgumentParser(description='Import and process M7700 JSON Graph files.')
-    parser.add_argument('filename', metavar='filename', type=str, nargs=1,
-                   help='JSON file for parsing')
-    parser.add_argument('--dot', "-d",  metavar='dotfile', type=str, nargs=2, #flag and filename
-                   help='--dot/-d dotfile: Convert to a graphviz dot flie')
-    parser.add_argument('--rom', "-r", metavar='filename', type=bool,  #flag and filename
-                   help='--rom/-r filename: Grab JSON for M7700 ROM')
 
-    json_file = parser.parse_args('filename')
-    dot_file = parser.parse_args('--dot,1')
-    rom_file = parser.parse_args('--rom')
+    parser.add_argument('filename',metavar='filename', nargs='?', type=argparse.FileType('r'), default=sys.stdin, 
+                   help='JSON file for parsing')
+
+    parser.add_argument('-d','--dot', metavar='dotfile', type=argparse.FileType('w'), default=sys.stdout,  #flag and filename
+                   help='Convert to a graphviz dot flie')
+    parser.add_argument('-r','--rom', metavar='filename', type=bool,  #flag and filename
+                   help='Grab JSON for M7700 ROM')
+
+    json_file = parser.parse_args()
+    dot_file = parser.parse_args()
+    rom_file = parser.parse_args()
     
     # parse each file name, attempt to pull JSON from files
     if rom_file:
-        parse_rom()
-
+        ret = parse_json(parse_rom(json_file))
     else:
         ret = parse_json(json_file)
         
@@ -44,7 +48,6 @@ def main ():
         #TODO: convert JSON to DOT, output
         # convert RET
         with open(outfile, "w") as f: #make a new .dot outfile
-            
             f.write(json.dumps(ret)) # dump graph, TODO: parse graph
             f.close()
 
