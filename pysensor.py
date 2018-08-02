94750e25d44cc1e2b8a830d09ef8fbcf65756b09
Greg <buthker.2@wright.edu>
2018-08-02 10:57:49 -0400

    Began R2pipe integration

--- pysensor.py
+++ pysensor.py
@@ -29,7 +29,46 @@ def parse_rom(infile):
     # TODO: include R_PIPE API, call R2 and get the JSON from the ROM
     print("Not implemented yet!")
     ret = infile
+
+    # first, call R2 and find candidates from the ROM
+    get_rom_candidates(infile)
+
+
     return ret
+
+def get_rom_candidates(infile):
+    print("Loading into R2...")
+
+    # load infile into R2
+    r2 = r2pipe.open(infile)
+
+    if r2: # assert the R2 file opened correctly
+        r2.cmd('aa') # analyze all
+        candidates = r2.cmd('/a call') # Use the /a call command in R2 to specify all R2 callees 
+        candidates = check_candidates(candidates)
+
+    # TODO: the rest of this
+    # Grab the function name of each of those calls
+
+    # Create the Caller dictionary with each main function of callees, made up of caller objects
+
+    # Each caller object has the addresses of what they call
+
+    # For each caller, the address is paired with the JSON of the corresponding function
+
+    # After the JSON is fully parsed, the data structure is returned to the PARSE_ROM func
+
+    else: 
+        print("Error parsing R2")
+
+    print("Not implemented yet!")
+    return 0
+
+# Purge all addresses that aren't within 5 lines of at least 1 other call, and number less than 5 total
+def check_candidates(candidates):
+    for candidate in candidates:
+        print ("Candidate: " + candidate)
+    return 0
 
 def main ():
     # set up the parser first
@@ -38,7 +77,7 @@ def main ():
     parser = argparse.ArgumentParser(description='Import and process M7700 JSON Graph files.')
 
     parser.add_argument('filename', metavar='filename', nargs='?', type=argparse.FileType('r'), default=sys.stdin, 
-                   help='M770 ROM file for parsing')
+                   help='M7700 ROM file for parsing')
 
     parser.add_argument('-j','--json', action="store_true", #flag and filename
                    help='Compare JSON method with another JSON tree, find similarities')
@@ -50,12 +89,12 @@ def main ():
     args = parser.parse_args()
     infile = args.filename
     #dot_file = args.dot
-    json_compare = args.rom
+    json_compare = args.json
 
     # parse each file name, attempt to pull JSON from files
-    if json_compare:
-        ret = parse_json(infile, None)
-    else:
+    if json_compare: # JSON file and comparison provided
+        ret = parse_json(infile, args.json)
+    else:            # do ROM-level analysis with R2pipe
         ret = parse_json(parse_rom(infile), None)
 
     print(ret)    
